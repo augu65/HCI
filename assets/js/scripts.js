238a7c5634b2b28b04a0be1b329a0751eeabda1a
Jonah Stegman <31897198+augu65@users.noreply.github.com>
2021-11-07 22:39:04 -0500

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -193,12 +193,12 @@
 		var xhr = new XMLHttpRequest();
 		xhr.open("POST", "https://enao4ux6542qagn.m.pipedream.net", true);
 		xhr.setRequestHeader('Content-Type', 'application/json');
-		xhr.send(JSON.stringify(data));
 		 xhr.onload = function (e) {
   if (xhr.readyState === 4) {
     if (xhr.status === 200) {
 			window.location.replace("/HCI/study.html")
   		  }}};
+		xhr.send(JSON.stringify(data));
 	});
 
     window.addEventListener('keydown', (event) => {
