4210347bd0f4aa6f6320fde0fdcebc8a221aad11
Jonah Stegman <31897198+augu65@users.noreply.github.com>
2021-11-19 13:37:46 -0500

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -226,7 +226,7 @@
 		const name = document.getElementById('name');
 		const sentence = document.getElementById('sentence');
 		const input2 = document.getElementById('sentence_input');
-		if(input.hidden == false){
+		if(input.hidden != false){
 			if(input.value==""){
 				alert("Please enter your first and last name in the field provided");
 				return;
