fb724f8401409547221f2fd0befe344046d3818b
Jonah Stegman <31897198+augu65@users.noreply.github.com>
2021-11-23 00:37:23 -0500

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -158,7 +158,7 @@
         let id = sessionStorage.getItem('times');
         // Setup transmit data
         var inputName = event.srcElement.attributes.name;
-        var sentenceId = inputName.replace("sentence","");
+        var sentenceId = inputName.toString().replace("sentence","");
         var uuid = select('#uuid').value;
         var altKey = event.altKey ? 1 : 0;
         var ctrlKey = event.ctrlKey ? 1 : 0;
